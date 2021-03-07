6f882ebbf715b3608a830fb6a95207c3571a904b
Gabriel Ibson <gabrielibson@gmail.com>
2021-03-07 19:35:12 -0300

    Add initial structure

---
+++ src/test/java/com/gabrielibson/jsondiff/JsondiffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielibson.jsondiff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsondiffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
